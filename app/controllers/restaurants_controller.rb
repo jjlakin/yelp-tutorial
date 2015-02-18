15786a35cadb5f5f86f8f1237ff5d8207122cf8c
jjlakin <j.lakin@gmail.com>
2015-02-18 10:45:26 +0000

    morning commit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,4 +16,13 @@ class RestaurantsController < ApplicationController
     params.require(:restaurant).permit(:name)
   end
 
+  def show
+  	@restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+  	@restaurant = Restaurant.find(params[:id])
+  end
+
+
 end
